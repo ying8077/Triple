e3e949c10df3f5887da9503f992304db965b021c
Ying Jou <tata12300807@gmail.com>
2022-12-21 05:30:02 +0800

    update sql

--- server/models/card_model.js
+++ server/models/card_model.js
@@ -2,27 +2,27 @@ const { pool } = require('./mysqlconn');
 
 const getPostThumbnail = async (postIds) => {
     const queryStr = 'SELECT `image` FROM `card` WHERE `post_id` IN(?) GROUP BY `post_id`';
-    const bindings = [postIds];
+    const bindings = [postIds+""];
     const [result] = await pool.query(queryStr, bindings);
     return result;
 };
 
 const getCollectionThumbnail = async (locationId) => {
     const queryStr = 'SELECT `image` FROM `card` WHERE `location_id` = ? limit 1';
-    const bindings = [locationId];
+    const bindings = [locationId+""];
     const [result] = await pool.query(queryStr, bindings);
     return result;
 };
 
 const getCards = async (postId) => {
     const queryStr = 'SELECT `location_id`, `description`, `recommend`, `image` FROM `card` WHERE `post_id` IN(?)';
-    const bindings = [postId];
+    const bindings = [postId+""];
     const [result] = await pool.query(queryStr, bindings);
     return result;
 }
 
 const createCard = async (card) => {
-    const [result] = await pool.query('INSERT INTO `card` SET ?', card);
+    const [result] = await pool.query('INSERT INTO `card` SET ?', card+"");
     return result.insertId;
 }
 
--- server/models/collection_model.js
+++ server/models/collection_model.js
@@ -1,22 +1,22 @@
 const { pool } = require('./mysqlconn');
 
 const createCollection = async (collection) => {
-    const [result] = await pool.query('INSERT INTO `collection` SET ?', collection);
+    const [result] = await pool.query('INSERT INTO `collection` SET ?', collection+"");
 }
 
 const getCollection = async (user_id) => {
-    const [result] = await pool.query('SELECT `id`, `name`, `details` FROM `collection` WHERE `user_id` = ?', user_id);
+    const [result] = await pool.query('SELECT `id`, `name`, `details` FROM `collection` WHERE `user_id` = ?', user_id+"");
     return result
 }
 
 const updateCollection = async (collection_id, details) => {
-    const [oldColletion] = await pool.query('SELECT `details` FROM `collection` WHERE id = ?', collection_id);
+    const [oldColletion] = await pool.query('SELECT `details` FROM `collection` WHERE id = ?', collection_id+"");
     const oldDetails = oldColletion[0].details;
     const index = oldDetails.indexOf(']');
     const newDetails = oldDetails.slice(0, index) + `,${details.substring(9)}`;
 
     const queryStr = 'UPDATE `collection` SET `details`= ? WHERE `id` = ?';
-    const bindings = [newDetails, collection_id];
+    const bindings = [newDetails+"", collection_id+""];
     const [result] = await pool.query(queryStr, bindings);
 }
 
